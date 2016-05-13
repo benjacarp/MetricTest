83448885b2b490ea7ae49c0f3409c771ce2bfcf7
Benjamin Andres Salas <benjamin.salas@globant.com>
2016-05-13 16:59:49 -0300

    timer example with log file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,26 +1,44 @@
-import com.codahale.metrics.*;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Slf4jReporter;
+import com.codahale.metrics.Timer;
+
 import java.util.concurrent.TimeUnit;
 
+import static com.codahale.metrics.MetricRegistry.name;
+
 public class App {
-    static final MetricRegistry metrics = new MetricRegistry();
+    static final MetricRegistry METRIC_REGISTRY = new MetricRegistry();
+
+
     public static void main(String args[]) {
         startReport();
-        Meter requests = metrics.meter("requests");
-        requests.mark();
-        wait5Seconds();
+
+        Timer timer = METRIC_REGISTRY.timer(name(App.class, "Example-Timer"));
+        Timer.Context context = timer.time();
+
+        for (int i = 0; i < 10; i++) {
+            wait1Second();
+        }
+
+        context.stop();
+
+        /*Meter requests = METRIC_REGISTRY.meter("requests");
+        requests.mark();*/
+
+//        wait1Second();
     }
 
     static void startReport() {
-        Slf4jReporter reporter = Slf4jReporter.forRegistry(metrics)
+        Slf4jReporter reporter = Slf4jReporter.forRegistry(METRIC_REGISTRY)
                 .convertRatesTo(TimeUnit.SECONDS)
                 .convertDurationsTo(TimeUnit.MILLISECONDS)
                 .build();
         reporter.start(1, TimeUnit.SECONDS);
     }
 
-    static void wait5Seconds() {
+    static void wait1Second() {
         try {
-            Thread.sleep(5*1000);
+            Thread.sleep(1000);
         }
         catch(InterruptedException e) {}
     }
